afd234bc84deaec37244995bb652bcb758184893
Pablo_Marzol <pablomarzol@yandex.com>
2025-11-19 13:26:53 +0000

    more files added after npm run build_2

---
+++ shared/schema/index.js
@@ -0,0 +1,6 @@
+// Export all schemas
+export * from './users.schema.js';
+export * from './trading.schema.js';
+export * from './swap.schema.js';
+export * from './ai.schema.js';
+export * from './hyperliquid.schema.js';
