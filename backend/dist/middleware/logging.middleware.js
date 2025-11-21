9194124bb794d6eadc1f31d74d86453c2e58231f
Pablo_Marzol <pablomarzol@yandex.com>
2025-11-21 00:55:10 +0000

    all needed changes

--- backend/dist/middleware/logging.middleware.js
+++
@@ -1,8 +0,0 @@
-export function requestLogger(req, res, next) {
-    const start = Date.now();
-    res.on('finish', () => {
-        const duration = Date.now() - start;
-        console.log(`${req.method} ${req.path} ${res.statusCode} - ${duration}ms`);
-    });
-    next();
-}
--- shared/schema/index.js
+++
@@ -1,6 +0,0 @@
-// Export all schemas
-export * from './users.schema.js';
-export * from './trading.schema.js';
-export * from './swap.schema.js';
-export * from './ai.schema.js';
-export * from './hyperliquid.schema.js';
